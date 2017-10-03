880d9ee747e140c8375d9429606bb11c1bc1a22c
Ben Hadfield <benjohnhadfield@gmail.com>
2017-10-03 17:32:59 +0100

    removed bill and added periodicity to charge

--- apps/charge/admin.py
+++ apps/charge/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Charge
+
+
+class ChargeAdmin(admin.ModelAdmin):
+    list_display = (
+        'created',
+        'charge_date',
+        'value',
+        'is_paid',
+        'period',
+        'is_active',
+    )
+
+    list_filter = ('is_paid', 'is_active', 'charge_date', 'value', 'title',)
+
+
+admin.site.register(Charge, ChargeAdmin)
--- apps/charge/models.py
+++ apps/charge/models.py
@@ -3,14 +3,37 @@ A Charge represents a single, one-off amount of money that should be split
 across multiple parties.
 """
 
+from datetime import date
+
 from django.db import models
 
-from utils.model.mixins import TimeStampedModel, PublicIdModel
+from apps.utils.model.mixins import TimeStampedModel, PublicIdModel
 
 
 class Charge(TimeStampedModel, PublicIdModel):
-    value = models.PositiveIntegerField()
+    PERIOD_CHOICES = (
+        (0, 'once'),
+        (1, 'daily'),
+        (2, 'monthly'),
+        (3, 'weekly'),
+        (4, 'bi-weekly'),
+        (5, 'yearly'),
+    )
+
+    # Detail fields.
+    value = models.PositiveIntegerField()  # Amount in pence.
+    charge_date = models.DateField(default=date.today)  # Date to send charge.
+    is_paid = models.BooleanField(default=False)
+    # Meta fields.
+    title = models.CharField(max_length=255, blank=True)
     message = models.TextField(blank=True)
+    # Repeating charge fields.
+    period = models.PositiveSmallIntegerField(default=0, choices=PERIOD_CHOICES)
+    end_date = models.DateField(blank=True, null=True)
+
+    def is_active(self):
+        today = date.today()
+        return (self.charge_date < today) and (self.end_date > today)
 
     def __str__(self):
         return 'Charge {}'.format(self.created)

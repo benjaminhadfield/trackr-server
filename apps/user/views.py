b23f473a8622625cd4c38afe4a4fe6eb428100fb
Ben Hadfield <benjohnhadfield@gmail.com>
2017-10-06 20:23:49 +0100

    current user cannot split with themselves

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 
 from rest_framework import viewsets
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 from .serializers import UserSerializer
 
@@ -8,3 +10,8 @@ from .serializers import UserSerializer
 class UserViewsets(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @list_route(methods=['GET'])
+    def me(self, request, *args, **kwargs):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)

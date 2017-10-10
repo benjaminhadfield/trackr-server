5dde6d6f0e32e5c202b6196df93f35b16b40ec38
Ben Hadfield <benjohnhadfield@gmail.com>
2017-10-10 16:13:59 +0100

    user serialiser returns short name

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(read_only=True, source='get_full_name')
+    name = serializers.CharField(read_only=True, source='get_short_name')
 
     class Meta:
         model = User
